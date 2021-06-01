7f59af868b1ea7f17caaeb567c35d06f977f35ea
Moritz 'Morty' Strübe <moritz.struebe@mathema.de>
2021-06-01 08:47:18 +0200

    Fix creating F5VPN-Link
    Testing using the JS-Console isn't sufficient. :(

--- f5vpnlinker.user.js
+++ f5vpnlinker.user.js
@@ -5,26 +5,26 @@
 // @version     1.0
 // @author      morty
 // @description See https://github.com/cmorty/f5vpn-login
+// @run-at document-idle
 // ==/UserScript==
 
 
-do {
-  matches = document.cookie.match('(?<=MRHSession=)[a-z0-9]*(?=;)');
-  if (matches === null) {
-    console.warn('Unable to find session ID.');
-    break;
-  }
+console.warn("This script is running with document.readyState: " + document.readyState);
 
-  sessionId = matches[0];
-  console.log(`Found session ID "${sessionId}".`);
-
+function makef5Link () {
+  
   var boxes = document.getElementsByClassName("favorite");
   var boxarr = Array.from(boxes);
   var vpnarr = boxarr.filter(b => b.innerText.includes("VPN"));
   if(vpnarr.length != 1) {
-    console.warn(`No or too many elements found: ${vpnarr.length}`);
-    break;
+    console.warn(`No or too many elements found (${vpnarr.length}).`);
+    // Try again later
+    if(vpnarr.length == 0) {
+      setTimeout(function() { makef5Link(sessionId); }, 1000);
+    }
+    return;
   }
+  
   var vbox = vpnarr[0];
   var newbox = vbox.cloneNode(true);
   // I didn't get this to work with js so I just wrapped everything into an a-tag.
@@ -33,5 +33,16 @@ do {
   link.appendChild(newbox);
   vbox.replaceWith(link);
 
-} while(0);
+} 
+
+var matches = document.cookie.match('(?<=MRHSession=)[a-z0-9]*(?=;)');
+
+if (matches === null) {
+  console.warn('Unable to find session ID.');
+} else {
+  var sessionId = matches[0];
+  console.log(`Found session ID "${sessionId}".`);
+  //Wait for website to load
+  setTimeout(function() { makef5Link(sessionId); }, 1000);
+}
 
